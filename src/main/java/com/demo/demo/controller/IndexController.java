1f8757f8d71d34bf71dd8f13b36116c2e61e8441
Alok Chaudhary <alokchaudhary@Aloks-MacBook-Pro.local>
2020-02-12 08:42:51 +0530

    Properties  based dependency injection closes#4

--- src/main/java/com/demo/demo/controller/IndexController.java
+++ src/main/java/com/demo/demo/controller/IndexController.java
@@ -1,13 +1,19 @@
 package com.demo.demo.controller;
 
+import com.demo.demo.service.IndexService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
+    @Autowired
+    IndexService indexService;
+
     @RequestMapping("/")
     public String showIndex(){
+        System.out.println("Count::"+indexService.getCount());
         return "Index";
     }
 }
